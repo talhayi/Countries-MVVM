de86ea89159bc534040c84b34bec8f8e397cae47
talhayi <talhayldrm23@gmail.com>
2022-06-26 03:32:46 +0300

    model class created

---
+++ app/src/main/java/com/example/countriesmvvm/model/Model.kt
@@ -0,0 +1,10 @@
+package com.example.countriesmvvm.model
+
+data class Country(
+    val countryName: String?,
+    val countryCapital: String?,
+    val countryRegion: String?,
+    val countryCurrency: String?,
+    val countryFlagUrl: String?,
+    val countryLanguage: String?
+)
