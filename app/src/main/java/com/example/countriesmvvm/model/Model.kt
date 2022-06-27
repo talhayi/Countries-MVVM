1a620a044fffe67dd94f3c78fd8486434e9617d0
talhayi <talhayldrm23@gmail.com>
2022-06-27 13:06:46 +0300

    service classes created

--- app/src/main/java/com/example/countriesmvvm/model/Model.kt
+++ app/src/main/java/com/example/countriesmvvm/model/Model.kt
@@ -1,10 +1,23 @@
 package com.example.countriesmvvm.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Country(
+    @SerializedName("name")
     val countryName: String?,
+
+    @SerializedName("capital")
     val countryCapital: String?,
+
+    @SerializedName("region")
     val countryRegion: String?,
+
+    @SerializedName("currency")
     val countryCurrency: String?,
+
+    @SerializedName("flag")
     val countryFlagUrl: String?,
+
+    @SerializedName("language")
     val countryLanguage: String?
 )
