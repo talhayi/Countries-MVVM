5deb03bc61abbd796148d9193b0db9595c659bce
talhayi <talhayldrm23@gmail.com>
2022-06-28 17:14:38 +0300

    CountryDao Interface created for ROOM

---
+++ app/src/main/java/com/example/countriesmvvm/service/CountryDao.kt
@@ -0,0 +1,22 @@
+package com.example.countriesmvvm.service
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.countriesmvvm.model.Country
+
+@Dao
+interface CountryDao {
+
+    @Insert
+    suspend fun insertAll (vararg countries : Country): List<Long>
+
+    @Query("SELECT * FROM country")
+    suspend fun getAllCountries(): List<Country>
+
+    @Query("SELECT * FROM country WHERE uuid = :countryId")
+    suspend fun getCountry(countryId : Int) : Country
+
+    @Query("DELETE FROM country")
+    suspend fun deleteAllCountries()
+}
