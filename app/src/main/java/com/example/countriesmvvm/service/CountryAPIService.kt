d81d3744f76fc67c948f535786962d690c3da1ce
talhayi <talhayldrm23@gmail.com>
2022-07-02 12:13:41 +0300

    okhttp interceptor added

--- app/src/main/java/com/example/countriesmvvm/service/CountryAPIService.kt
+++ app/src/main/java/com/example/countriesmvvm/service/CountryAPIService.kt
@@ -2,21 +2,33 @@ package com.example.countriesmvvm.service
 
 import com.example.countriesmvvm.model.Country
 import io.reactivex.Single
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class CountryAPIService {
 
+
+
+    private val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
     private val BASE_URL = "http://yazilimgunlukleri.com/"
     private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
         .create(CountryAPI::class.java)
 
+
     fun getData() : Single<List<Country>>{
         return api.getCountries()
     }
+
+
 }
+
